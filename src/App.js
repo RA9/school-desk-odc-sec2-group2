3aff1b4f9761128b70d77aaa19466fdd62709272
Abraham MB Selebay <xyberkid0@gmail.com>
2022-12-04 14:17:19 +0000

    inital commit

--- src/App.js
+++ src/App.js
@@ -128,7 +128,6 @@ export default function App() {
         </div>
 
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-
           <div className="grid gap-5 row-gap-5 mb-8 lg:grid-cols-4 sm:grid-cols-2">
             <a
               href="/"
