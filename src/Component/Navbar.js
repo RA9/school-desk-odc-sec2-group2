191b3da1d9c6a92406ac717148594a6fafdab142
Abraham MB Selebay <xyberkid0@gmail.com>
2022-12-05 17:54:07 +0000

    initial commit

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -54,6 +54,21 @@ export default function Navbar() {
                     <div class="space-y-1 pt-2 pb-4">
                         {/* <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" --> */}
                         <Link
+                            to="/"
+                            class="
+                  block
+                  border-l-4 border-transparent
+                  py-2
+                  pl-3
+                  pr-4
+                  text-base
+                  font-medium
+                  text-gray-500
+                  hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700
+                ">
+                            Navbar
+                        </Link>
+                        <Link
                             to="/notification"
                             class="
                   block
