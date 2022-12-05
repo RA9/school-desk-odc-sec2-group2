e77144775f9151ff1d72126157f7b747b26d7b08
I. Jose Kpadeh <ijosekpadeh@gmail.com>
2022-12-05 13:45:40 +0000

    initial commit

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -123,6 +123,32 @@ export default function Contact(){
 
             </div>
         </form>
+
+        <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+          <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+              <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-8" alt="Flowbite Logo" />
+              <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SchoolDesk</span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+              <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">Home</a>
+              </li>
+              <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Notification</a>
+              </li>
+              <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">About Us</a>
+              </li>
+              <li>
+                <a href="#" class="hover:underline">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-2" />
+          <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">SchoolDesk™</a>. All Rights Reserved.
+          </span>
+        </footer>
         </>
     );
 };
